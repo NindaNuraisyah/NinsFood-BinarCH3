acb8b5b8e1dfe3998bc2332834c79b366de508c8
Ninda_Nuraisyah <nindanuraisyah0903@gmail.com>
2023-10-08 21:45:06 +0700

    finish feature cart list

---
+++ app/src/main/java/com/catnip/ninsfood_binarch3/presentation/feature/cart/CartViewModel.kt
@@ -0,0 +1,33 @@
+package com.catnip.ninsfood_binarch3.presentation.feature.cart
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.catnip.ninsfood_binarch3.data.repository.CartRepository
+import com.catnip.ninsfood_binarch3.model.Cart
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.collect
+
+class CartViewModel(private val repo: CartRepository) : ViewModel() {
+
+    val cartList = repo.getUserCartData().asLiveData(Dispatchers.IO)
+
+    fun increaseCart(item: Cart) {
+        viewModelScope.launch { repo.increaseCart(item).collect() }
+    }
+
+    fun decreaseCart(item: Cart) {
+        viewModelScope.launch {repo.decreaseCart(item).collect()}
+    }
+
+    fun deleteCart(item: Cart) {
+        viewModelScope.launch { repo.deleteCart(item).collect() }
+    }
+
+    fun setCartNotes(item: Cart) {
+        viewModelScope.launch { repo.setCartNotes(item).collect() }
+    }
+
+
+}
