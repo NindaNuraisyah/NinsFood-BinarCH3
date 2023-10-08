56891cf470bb0eb2c5814b2aa329a7e743dca6ad
Ninda_Nuraisyah <nindanuraisyah0903@gmail.com>
2023-10-08 23:28:16 +0700

    finish feature profile page

--- app/src/main/java/com/catnip/ninsfood_binarch3/presentation/feature/profile/ProfileFragment.kt
+++ app/src/main/java/com/catnip/ninsfood_binarch3/presentation/feature/profile/ProfileFragment.kt
@@ -6,13 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.catnip.ninsfood_binarch3.R
+import com.catnip.ninsfood_binarch3.databinding.FragmentProfileBinding
+
 class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+
 }
